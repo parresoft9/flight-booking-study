8844c64082014e37953e168ec00053da4068e948
Alejandro Parreño <plexus.aparreno@iberia.es>
2025-10-02 17:28:25 +0200

    init base project

---
+++ flight-booking-base/src/main/java/com/iberia/flightbooking/FlightBookingBaseApplication.java
@@ -0,0 +1,13 @@
+package com.iberia.flightbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBookingBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightBookingBaseApplication.class, args);
+	}
+
+}
---
+++ flight-booking-base/src/test/java/com/iberia/flightbooking/FlightBookingBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iberia.flightbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
